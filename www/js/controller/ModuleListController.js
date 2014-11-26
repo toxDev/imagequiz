d58a4918d9cca8c6f64426ee9372f3e941235ce1
toxic2302 <ni1703fl>
2014-11-26 22:09:08 +0100

    added search function

--- www/js/controller/ModuleListController.js
+++ www/js/controller/ModuleListController.js
@@ -2,4 +2,12 @@
 angular.module('imageQuizz').controller('ModuleListController',
     function () {
 
+        this.searchActive = false;
+
+        this.toggleSearch = function () {
+            if (this.searchActive) {
+                this.searchQuery = '';
+            }
+            this.searchActive = !this.searchActive;
+        }
     });
