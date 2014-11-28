d1660cabf7baaf75548dc5c7b273ecd074590134
Florian Kolb <florian.kolb@mni.thm.de>
2014-11-28 18:29:43 +0100

    fix search function

--- www/js/controller/ModuleListController.js
+++ www/js/controller/ModuleListController.js
@@ -1,9 +1,22 @@
 'use strict';
 angular.module('imageQuizz').controller('ModuleListController',
-    function () {
+    function ($scope) {
+
+        var thisSt = this;
+        //Für Zustandswechsel anmelden
+        $scope.$on('$stateChangeStart',
+            function (event) {
+                if (thisSt.searchActive == true) {
+                    var saveSearchQuery = localStorage.setItem('saveQuery', JSON.stringify(thisSt.searchQuery));
+                }
+            });
 
         this.searchActive = false;
-
+        if (localStorage.getItem('saveQuery')) {
+            this.searchActive = true;
+            this.searchQuery = JSON.parse(localStorage.getItem('saveQuery'));
+            localStorage.removeItem('saveQuery');
+        }
         this.toggleSearch = function () {
             if (this.searchActive) {
                 this.searchQuery = '';
