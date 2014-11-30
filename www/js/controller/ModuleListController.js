df2462648cab19806689e7f3c9af2bd2fc7ccc24
Florian Kolb <florian.kolb@mni.thm.de>
2014-11-30 20:02:37 +0100

    testing alphabetical list

--- www/js/controller/ModuleListController.js
+++ www/js/controller/ModuleListController.js
@@ -1,7 +1,67 @@
 'use strict';
 angular.module('imageQuizz').controller('ModuleListController',
-    function ($scope) {
+    function ($scope, $stateParams, $location, $ionicScrollDelegate) {
+        /*$scope.categories  = [   {"_id": "1", "name": "Automobil"}, {"_id": "2", "name": "Deutsche Sehenswürdigkeiten"},{"_id": "3", "name": "Magische Orte"},{"_id": "4", "name": "Geografie & Reisen"}];
+         var categories = $scope.categories;
+         var log = [];
 
+         $scope.alphabet = iterateAlphabet();
+
+         //Sort categories list by first letter of name
+         var tmp={};
+         for(i=0;i<categories.length;i++){
+         var letter=categories[i].name.toUpperCase().charAt(0);
+         if( tmp[ letter] ==undefined){
+         tmp[ letter]=[]
+         }
+         tmp[ letter].push( categories[i] );
+         }
+         $scope.sorted_categories = tmp;
+
+         //Click letter event
+         $scope.gotoList = function(id){
+         $location.hash(id);
+         $ionicScrollDelegate.anchorScroll();
+         }
+
+         //Create alphabet object
+         function iterateAlphabet()
+         {
+         var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+         var numbers = new Array();
+         for(var i=0; i<str.length; i++)
+         {
+         var nextChar = str.charAt(i);
+         numbers.push(nextChar);
+         }
+         return numbers;
+         }$scope.groups = [];
+         for (var i=0; i<10; i++) {
+         $scope.groups[i] = {
+         name: i,
+         items: []
+         };
+         for (var j=0; j<3; j++) {
+         $scope.groups[i].items.push(i + '-' + j);
+         }
+         }
+
+         */
+        /*
+         * if given group is the selected group, deselect it
+         * else, select the given group
+         */
+        /*
+         $scope.toggleGroup = function(group) {
+         if ($scope.isGroupShown(group)) {
+         $scope.shownGroup = null;
+         } else {
+         $scope.shownGroup = group;
+         }
+         };
+         $scope.isGroupShown = function(group) {
+         return $scope.shownGroup === group;
+         };*/
         var thisSt = this;
         //Für Zustandswechsel anmelden
         $scope.$on('$stateChangeStart',
