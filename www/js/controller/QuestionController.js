46709b2b1de713cc214a670bf5c4f10599f27d2b
Julian Schmitt <julian.schmitt@mni.thm.de>
2014-12-02 09:30:51 +0100

    buttons mit ng-repeat

--- www/js/controller/QuestionController.js
+++ www/js/controller/QuestionController.js
@@ -1,5 +1,16 @@
 'use strict';
 angular.module('imageQuizz').controller('QuestionController',
     function () {
+         var Question = function (name) {
+            this.name = name;
+        };
 
+        var answers = [];
+
+        answers.push(new Question('Antwort 1'));
+        answers.push(new Question('Antwort 2'));
+        answers.push(new Question('Antwort 3'));
+        answers.push(new Question('Antwort 4'));
+
+        this.answers = answers;
     });
