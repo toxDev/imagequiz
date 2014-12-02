06c4b4001a32b66adfcb0fa8b7fc9cf8109781c0
Andreas Ebner <andreas.ebner@mni.thm.de>
2014-12-02 10:55:27 +0100

    stateParams in QuestionLit added

--- www/js/controller/QuestionListController.js
+++ www/js/controller/QuestionListController.js
@@ -1,11 +1,10 @@
 'use strict';
 angular.module('imageQuizz').controller('QuestionListController',
-    function (QuestionData, $scope, $state) {
-        $scope.questionList = QuestionData.findAllQuestions();
-        console.log($scope.questionList);
+    function (QuestionData, $scope, $state, $stateParams) {
+        $scope.questionList = QuestionData.findAllQuestionsByCategory($stateParams.id);
 
         $scope.startQuizzMode = function () {
-            $state.go('question_view_quizz', {id: questionList[0].category})
+            $state.go('question_view_quizz', {id: $scope.questionList[0].category});
         }
 
     });
