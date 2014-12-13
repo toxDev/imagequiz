56ff3ada4547d6657126febe5b9d1afb530d4575
Julian Schmitt <julian.schmitt@mni.thm.de>
2014-12-13 13:23:54 +0100

    single question mode: added logic

--- www/js/controller/QuestionController.js
+++ www/js/controller/QuestionController.js
@@ -1,6 +1,36 @@
 'use strict';
 angular.module('imageQuizz').controller('QuestionController',
-    function (QuestionData,$scope,$stateParams) {
+    function (QuestionData,$scope,$stateParams,$ionicPopup,$ionicNavBarDelegate) {
 
         $scope.question = QuestionData.findQuestionById($stateParams.id);
+
+        this.testAnswer = function (answer) {
+            console.log(answer);
+            var correctAnswer;
+
+            $scope.question.options.forEach(function (option) {
+                if(option['answer'] == true) {
+                    correctAnswer = option['option'];
+                }
+            });
+
+            console.log(correctAnswer);
+
+            var result;
+
+            if(answer === correctAnswer){
+                result = 'Richtig! Sehr gut :)'
+            } else {
+                result = 'Leider Falsch :('
+            }
+
+            var popup = $ionicPopup.alert({
+                title:result,
+                template: 'Deine Antwort: ' + answer +'<br>'+'Richtige Antwort: ' + correctAnswer
+            });
+
+            popup.then(function () {
+                $ionicNavBarDelegate.back()
+            })
+        }
     });
