40f5158a0280e3a9e7314fb7f0dce1c4c6b7c089
Florian Kolb <florian.kolb@mni.thm.de>
2014-12-13 18:07:05 +0100

    fixed search

--- www/js/controller/ModuleListController.js
+++ www/js/controller/ModuleListController.js
@@ -2,9 +2,23 @@
 angular.module('imageQuizz').controller('ModuleListController',
     function ($scope, QuestionData) {
 
-        var letters = $scope.letters = [];
-        var currentCharCode = $scope.currentCharCode = 'A'.charCodeAt(0) - 1;
         var thisSt = this;
+        var modules = QuestionData.findAllQuestions();
+        /*[{"id": 1, "category": "Autos"},
+         {"id": 2, "category": "Gefährliche Raubtiere"},
+         {"id": 3, "category": "Deutsche Sehenswürdigkeiten"},
+         {"id": 4, "category": "Magische Orte"}];*/
+
+        var tmp = {};
+        for (var i = 0; i < modules.length; i++) {
+            var letter = modules[i].category.charAt(0);
+            if (tmp[letter] == undefined) {
+                tmp[letter] = []
+            }
+            tmp[letter].push(modules[i]);
+        }
+
+        $scope.repeaterObject = tmp;
 
         //Für Zustandswechsel anmelden
         $scope.$on('$stateChangeStart',
@@ -26,11 +40,83 @@ angular.module('imageQuizz').controller('ModuleListController',
             }
             this.searchActive = !this.searchActive;
         }
-
-        $scope.modules = QuestionData.findAllCategorys();
-
-        $scope.getItemHeight = function (item, index) {
-            //Make evenly indexed items be 10px taller, for the sake of example
-            return (index % 2) === 0 ? 50 : 60;
-        };
     });
+
+/*window.MODULES =
+ [{"id": 1, "category": "Autos"},
+ {"id": 2, "category": "Gefährliche Raubtiere"},
+ {"id": 3, "category": "Deutsche Sehenswürdigkeiten"},
+ {"id": 4, "category": "Magische Orte"}];*/
+
+/*var letters = $scope.letters = [];
+ var modules = $scope.modules = [];
+ var currentCharCode = $scope.currentCharCode = 'A'.charCodeAt(0) - 1;
+ //var thisSt = this;
+
+ window.MODULES.sort(function (a, b) {
+ return a.category > b.category ? 1 : -1;
+ }).forEach(function (Modul) {
+ //get the letter of the module
+ var modulCharCode = Modul.category.toUpperCase().charCodeAt(0);
+ //
+ var difference = modulCharCode - currentCharCode;
+ for (var i = 1; i <= difference; i++) {
+ addLetter(currentCharCode + i);
+ }
+ currentCharCode = modulCharCode;
+ modules.push(Modul);
+ });
+
+ //If names ended before Z, add everything up to Z
+ for (var i = currentCharCode + 1; i <= 'Z'.charCodeAt(0); i++) {
+ addLetter(i);
+ }
+
+ function addLetter(code) {
+ var letter = String.fromCharCode(code);
+ modules.push({
+ isLetter: true,
+ letter: letter
+ });
+ letters.push(letter);
+ //console.log(letters);
+ }
+
+ //Letters are shorter, everything else is 52 pixels
+ $scope.getItemHeight = function (item) {
+ return item.isLetter ? 40 : 100;
+ };
+ $scope.getItemWidth = function (item) {
+ return '100%';
+ };
+
+ var letterHasMatch = {};
+ $scope.getModules = function () {
+ letterHasMatch = {};
+ //Filter contacts by $scope.search.
+ //Additionally, filter letters so that they only show if there
+ //is one or more matching contact
+ return modules.filter(function (item) {
+ var itemDoesMatch = !$scope.search || item.isLetter ||
+ item.category.toLowerCase().indexOf($scope.search.toLowerCase()) > -1;
+
+ //Mark this person's last name letter as 'has a match'
+ if (!item.isLetter && itemDoesMatch) {
+ var letter = item.category.charAt(0).toUpperCase();
+ letterHasMatch[letter] = true;
+ }
+
+ return itemDoesMatch;
+ }).filter(function (item) {
+ //Finally, re-filter all of the letters and take out ones that don't
+ //have a match
+ if (item.isLetter && !letterHasMatch[item.letter]) {
+ return false;
+ }
+ return true;
+ });
+ };
+
+ $scope.clearSearch = function () {
+ $scope.search = '';
+ };});*/
