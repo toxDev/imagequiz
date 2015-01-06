fec1bb7351ae3ff68d6c8f2fb0f9c0383767eb5f
Julian Schmitt <julian.schmitt@mni.thm.de>
2015-01-06 11:41:41 +0100

    added Persist Service

---
+++ www/js/service/Persist.js
@@ -0,0 +1,24 @@
+'use strict';
+angular.module('imageQuizz').factory(
+    function ($firebase, FIREBASE_URL) {
+
+        var rootRef = new Firebase(FIREBASE_URL);
+        var userRef = rootRef.child(localStorage.getItem('uid'));
+        var userRefNg = $firebase(userRef);
+
+        var service = {
+            findAll: function () {
+                return userRefNg.$asArray();
+            },
+            update: function (stat) {
+                this.findAll().$save(stat);
+            },
+            persist: function (stat) {
+                this.findAll().$add(stat);
+            },
+            findById: function (id) {
+                return this.findAll().$getRecord(id);
+            }
+        };
+    });
+
